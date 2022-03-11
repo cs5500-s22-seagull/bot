97eab54dc6e72441ff891a444e06995d4249bdf1
shanmingyuan <mshan@u.rochester.edu>
2022-03-10 18:32:36 -0800

    Disable MongoDB

--- src/main/java/edu/northeastern/cs5500/starterbot/repository/RepositoryModule.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/repository/RepositoryModule.java
@@ -8,17 +8,17 @@ import edu.northeastern.cs5500.starterbot.model.UserPreference;
 public class RepositoryModule {
     // NOTE: You can use the following lines if you'd like to store objects in memory.
     // NOTE: The presence of commented-out code in your project *will* result in a lowered grade.
-    // @Provides
-    // public GenericRepository<UserPreference> provideUserPreferencesRepository(
-    //         InMemoryRepository<UserPreference> repository) {
-    //     return repository;
-    // }
-
     @Provides
     public GenericRepository<UserPreference> provideUserPreferencesRepository(
-            MongoDBRepository<UserPreference> repository) {
+            InMemoryRepository<UserPreference> repository) {
         return repository;
     }
+
+    // @Provides
+    // public GenericRepository<UserPreference> provideUserPreferencesRepository(
+    //         MongoDBRepository<UserPreference> repository) {
+    //     return repository;
+    // }
 
     @Provides
     public Class<UserPreference> provideUserPreference() {
