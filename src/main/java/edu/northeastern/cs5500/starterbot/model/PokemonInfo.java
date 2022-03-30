7e576107b38e5629e91722aa0343c133de4dade5
FlashUnlimited2019 <crh98106@gmail.com>
2022-03-30 16:14:20 -0700

    delete allargsconstructor

--- src/main/java/edu/northeastern/cs5500/starterbot/model/PokemonInfo.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/model/PokemonInfo.java
@@ -1,14 +1,12 @@
 package edu.northeastern.cs5500.starterbot.model;
 
 import java.util.List;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class PokemonInfo implements Model {
 
     private ObjectId id;
