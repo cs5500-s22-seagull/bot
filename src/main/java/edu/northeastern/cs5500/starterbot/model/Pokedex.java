ad7464f05ff642d1be9e438b4d4db3823d68f5f8
FlashUnlimited2019 <crh98106@gmail.com>
2022-03-30 15:33:47 -0700

    BigFix: add pictureAddress and maxHP to pokemoninfo

--- src/main/java/edu/northeastern/cs5500/starterbot/model/Pokedex.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/model/Pokedex.java
@@ -1,11 +1,13 @@
 package edu.northeastern.cs5500.starterbot.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Pokedex implements Model {
     private ObjectId id;
     private int seen;
--- src/main/java/edu/northeastern/cs5500/starterbot/model/PokemonInfo.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/model/PokemonInfo.java
@@ -1,12 +1,14 @@
 package edu.northeastern.cs5500.starterbot.model;
 
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class PokemonInfo implements Model {
 
     private ObjectId id;
@@ -16,4 +18,6 @@ public class PokemonInfo implements Model {
     private List<String> pokemonType;
     private List<ObjectId> evolution;
     private String introduction;
+    private String pictureAddress;
+    private int maxHP;
 }
