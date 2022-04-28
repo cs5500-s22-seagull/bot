27c04471e79f372fe4409751428679286e3b2845
shanmingyuan <mshan@u.rochester.edu>
2022-04-27 17:07:34 -0700

    removed button referrences

--- src/main/java/edu/northeastern/cs5500/starterbot/listener/MessageListener.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/listener/MessageListener.java
@@ -1,13 +1,11 @@
 package edu.northeastern.cs5500.starterbot.listener;
 
-import edu.northeastern.cs5500.starterbot.buttonHandler.ButtonHandler;
 import edu.northeastern.cs5500.starterbot.command.Command;
 import edu.northeastern.cs5500.starterbot.selectionHandler.SelectionHandler;
 import java.util.Collection;
 import java.util.Set;
 import java.util.stream.Collectors;
 import javax.inject.Inject;
-import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 import net.dv8tion.jda.api.events.interaction.SelectionMenuEvent;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -16,8 +14,6 @@ import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 public class MessageListener extends ListenerAdapter {
 
     @Inject Set<Command> commands;
-
-    @Inject Set<ButtonHandler> buttonHandlers;
 
     @Inject Set<SelectionHandler> selectionHandlers;
 
@@ -38,16 +34,6 @@ public class MessageListener extends ListenerAdapter {
 
     public Collection<CommandData> allCommandData() {
         return commands.stream().map(Command::getCommandData).collect(Collectors.toList());
-    }
-
-    @Override
-    public void onButtonClick(ButtonClickEvent event) {
-        for (ButtonHandler buttonHandler : buttonHandlers) {
-            if (buttonHandler.getName().equals(event.getComponentId())) {
-                buttonHandler.onEvent(event);
-                return;
-            }
-        }
     }
 
     @Override
